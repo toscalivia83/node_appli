6fc34bf687f1c9070ac85ba46604580a38d77eeb
Marion Gourlaouen <marion.gourlaouen@isen.fr>
2016-08-03 15:41:00 +0200

    Authorize csv

--- user.js
+++ user.js
@@ -10,7 +10,6 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Content-Disposition, Accept')
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS, Content-Disposition')
-  res.header('Content-Disposition', 'attachment; filename="export.csv"')
   next()
 })
 
